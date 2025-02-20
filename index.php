b25552ccb485ccb14672fdb3a5399ab057dfdc76
paveld <pavel.termhg@gmail.com>
2025-02-20 23:14:02 +0200

    add lab info to readme

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+
+$days = 333;
+$message = "i love python(just jocking)!";
+
+echo "$message through $days days <br>";
+echo "$message through $days days\n";
+
+echo PHP_INT_MAX;
+
